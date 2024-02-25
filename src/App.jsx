8f1294f69a7a476058c16795383233fd5bfa04dc
Nick Neessen <nick.neessen@gmail.com>
2024-02-25 15:07:33 -0600

    using as in styled components

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,7 @@ import styled from "styled-components";
 import GlobalStyles from "./styles/GlobalStyles";
 import Button from "./ui/Button";
 import Input from "./ui/Input";
-
-const H1 = styled.h1`
-  font-size: 50px;
-  font-weight: 600;
-`;
+import Heading from "./ui/Heading";
 
 const StyledApp = styled.div`
   background-color: orangered;
@@ -18,9 +14,15 @@ function App() {
     <>
       <GlobalStyles /> {/* doesn't accept children */}
       <StyledApp>
-        <H1>The Wild Oasis</H1>
+        <Heading as="h1">The Wild Oasis</Heading>
+
+        <Heading as="h2">Check in and out</Heading>
+
         <Button onClick={() => alert("test")}>Check in</Button>
         <Button onClick={() => alert("test")}>Check out</Button>
+
+        <Heading as="h3">Form</Heading>
+
         <Input type="number" placeholder="Number of guests" />
       </StyledApp>
     </>
