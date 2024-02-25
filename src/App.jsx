2cbb433bdd7aa048b94731712326f6ca51cd27bf
Nick Neessen <nick.neessen@gmail.com>
2024-02-25 15:22:49 -0600

    a

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,10 @@ import GlobalStyles from "./styles/GlobalStyles";
 import Button from "./ui/Button";
 import Input from "./ui/Input";
 import Heading from "./ui/Heading";
+import Row from "./ui/Row";
 
 const StyledApp = styled.div`
-  background-color: orangered;
+  /* background-color: orangered; */
   padding: 20px;
 `;
 
@@ -14,16 +15,31 @@ function App() {
     <>
       <GlobalStyles /> {/* doesn't accept children */}
       <StyledApp>
-        <Heading as="h1">The Wild Oasis</Heading>
+        <Row>
+          <Row type="horizontal">
+            <Heading as="h1">The Wild Oasis</Heading>
 
-        <Heading as="h2">Check in and out</Heading>
+            <div>
+              <Heading as="h2">Check in and out</Heading>
+              <Button onClick={() => alert("test")}>Check in</Button>
+              <Button
+                variation="secondary"
+                size="small"
+                onClick={() => alert("test")}
+              >
+                Check out
+              </Button>
+            </div>
+          </Row>
 
-        <Button onClick={() => alert("test")}>Check in</Button>
-        <Button onClick={() => alert("test")}>Check out</Button>
-
-        <Heading as="h3">Form</Heading>
-
-        <Input type="number" placeholder="Number of guests" />
+          <Row>
+            <Heading as="h3">Form</Heading>
+            <form>
+              <Input type="number" placeholder="Number of guests" />
+              <Input type="number" placeholder="Number of guests" />
+            </form>
+          </Row>
+        </Row>
       </StyledApp>
     </>
   );
