be990e138af1375a8df34d584a8c6926eac7fca3
Nick Neessen <nick.neessen@gmail.com>
2024-02-25 11:52:59 -0600

    learned about styled components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,47 @@
+import styled from "styled-components";
+
+const H1 = styled.h1`
+  font-size: 50px;
+  font-weight: 600;
+`;
+
+const Button = styled.button`
+  font-size: 1.4rem;
+  padding: 1.2rem 1.6rem;
+  margin: 1rem;
+  font-weight: 500;
+  border: none;
+  border-radius: 7px;
+  background-color: purple;
+  color: white;
+  cursor: pointer;
+  /* &:hover,
+  &:focus {
+    color: greenyellow;
+    background-color: blueviolet;
+  } */
+`;
+
+const Input = styled.input`
+  border: 1px solid #ddd;
+  border-radius: 5px;
+  padding: 0.8rem 1.2rem;
+`;
+
+const StyledApp = styled.div`
+  background-color: orangered;
+  padding: 20px;
+`;
+
 function App() {
-  return <div>Hello Bitch!</div>;
+  return (
+    <StyledApp>
+      <H1>The Wild Oasis</H1>
+      <Button onClick={() => alert("test")}>Check in</Button>
+      <Button onClick={() => alert("test")}>Check out</Button>
+      <Input type="number" placeholder="Number of guests" />
+    </StyledApp>
+  );
 }
 
 export default App;
