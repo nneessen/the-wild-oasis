0d75a1da86a273edcc8ba00ab7e6639cf6c84c6c
Nick Neessen <nick.neessen@gmail.com>
2024-02-26 10:03:19 -0600

    implement queryclient

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,22 +1,19 @@
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
-import { useEffect } from "react";
-import { getCabins } from "../services/apiCabins";
+
+import CabinTable from "../features/cabins/CabinTable";
 
 function Cabins() {
-  useEffect(function () {
-    getCabins().then((data) => console.log(data));
-  }, []);
-
   return (
-    <Row type="horizontal">
-      <Heading as="h1">All cabins</Heading>
-      <p>TEST</p>
-      <img
-        src="https://ghmlxidticyjldrjqqer.supabase.co/storage/v1/object/public/cabin-images/cabin-001.jpg?t=2024-02-26T14%3A01%3A12.509Z"
-        alt=""
-      />
-    </Row>
+    <>
+      <Row type="horizontal">
+        <Heading as="h1">All cabins</Heading>
+      </Row>
+
+      <Row>
+        <CabinTable />
+      </Row>
+    </>
   );
 }
 
