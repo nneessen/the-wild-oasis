82fb0a5307eeae642c22a5701486772006bbba5d
Nick Neessen <nick.neessen@gmail.com>
2024-02-27 15:21:30 -0600

    uploading files to supabase

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://ghmlxidticyjldrjqqer.supabase.co";
+export const supabaseUrl = "https://ghmlxidticyjldrjqqer.supabase.co";
 const supabaseKey =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdobWx4aWR0aWN5amxkcmpxcWVyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDg5MDgyMzQsImV4cCI6MjAyNDQ4NDIzNH0.1UXURqaZDemw7WodqY7yBQKxhg79hV-e8I4oi2eA9PM";
 const supabase = createClient(supabaseUrl, supabaseKey);
