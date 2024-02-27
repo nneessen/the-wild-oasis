2269e62b09da3f023f1f3deb8a0b37d164a517be
Nick Neessen <nick.neessen@gmail.com>
2024-02-27 07:56:07 -0600

    useForm register

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,9 +1,14 @@
+/* eslint-disable react/jsx-no-leaked-render */
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
 
 import CabinTable from "../features/cabins/CabinTable";
+import { useState } from "react";
+import Button from "../ui/Button";
+import CreateCabinForm from "../features/cabins/CreateCabinForm";
 
 function Cabins() {
+  const [showForm, setShowFrom] = useState(false);
   return (
     <>
       <Row type="horizontal">
@@ -12,6 +17,11 @@ function Cabins() {
 
       <Row>
         <CabinTable />
+
+        <Button onClick={() => setShowFrom((show) => !show)}>
+          Add new cabin
+        </Button>
+        {showForm && <CreateCabinForm />}
       </Row>
     </>
   );
