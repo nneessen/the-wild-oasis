2de7b8269b5c7a9d3657da1dc81c866e7fac28c0
Nick Neessen <nick.neessen@gmail.com>
2024-02-28 18:32:33 -0600

    created a custom useOutsideClick hook to be used for modals

---
+++ src/hooks/useOutsideClick.js
@@ -0,0 +1,23 @@
+import { useContext, useEffect, useRef } from "react";
+import { ModalContext } from "../ui/Modal";
+
+export function useOutsideClick(handler, listenCapture = true) {
+  const ref = useRef();
+  useEffect(
+    function () {
+      function handleClick(e) {
+        if (ref.current && !ref.current.contains(e.target)) {
+          handler();
+        }
+      }
+
+      document.addEventListener("click", handleClick, listenCapture);
+      // must pass 'true' in order to be able to reclick again on the show modal button
+
+      return () =>
+        document.removeEventListener("click", handleClick, listenCapture);
+    },
+    [handler, listenCapture]
+  );
+  return { ref, handler };
+}
--- src/ui/Modal.jsx
+++ src/ui/Modal.jsx
@@ -1,7 +1,15 @@
-import { cloneElement, createContext, useContext, useState } from "react";
+import {
+  cloneElement,
+  createContext,
+  useContext,
+  useEffect,
+  useRef,
+  useState,
+} from "react";
 import { createPortal } from "react-dom";
 import { HiXMark } from "react-icons/hi2";
 import styled from "styled-components";
+import { useOutsideClick } from "../hooks/useOutsideClick";
 
 const StyledModal = styled.div`
   position: fixed;
@@ -53,7 +61,7 @@ const Button = styled.button`
   }
 `;
 
-const ModalContext = createContext();
+export const ModalContext = createContext();
 
 function Modal({ children }) {
   const [openName, setOpenName] = useState("");
@@ -76,12 +84,14 @@ function Open({ children, opens: opensWindowName }) {
 
 function Window({ children, name }) {
   const { openName, close } = useContext(ModalContext);
+  const { ref, handler } = useOutsideClick(close);
+
   if (name !== openName) return null;
 
   return createPortal(
     <Overlay>
-      <StyledModal>
-        <Button onClick={close}>
+      <StyledModal ref={ref}>
+        <Button onClick={handler}>
           <HiXMark />
         </Button>
         <div>{cloneElement(children, { onCloseModal: close })}</div>
