99b44d5ea0b61853243ad3457bbc8725c1415e35
Nick Neessen <nick.neessen@gmail.com>
2024-02-29 15:25:10 -0600

    more sorting stuff

--- src/features/bookings/useBookings.js
+++ src/features/bookings/useBookings.js
@@ -12,13 +12,18 @@ export function useBookings() {
       ? null
       : { field: "status", value: filterValue };
 
+  // SORT
+  const sortByRaw = searchParams.get("sortBy") || "startDate-desc";
+  const [field, direction] = sortByRaw.split("-");
+  const sortBy = { field, direction };
+
   const {
     isLoading,
     data: bookings,
     error,
   } = useQuery({
-    queryKey: ["bookings", filter], // basically a dependancy array of useQuery. if these values change, it will re-fetch the data
-    queryFn: () => getBookings({ filter }),
+    queryKey: ["bookings", filter, sortBy], // basically a dependancy array of useQuery. if these values change, it will re-fetch the data
+    queryFn: () => getBookings({ filter, sortBy }),
   });
   return { isLoading, error, bookings };
 }
