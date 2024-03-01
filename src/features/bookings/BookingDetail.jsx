f5996783b89f3fe7be9aa768e7ccef1d65c2a56d
Nick Neessen <nick.neessen@gmail.com>
2024-03-01 07:36:58 -0600

    implemented checkin capability

--- src/features/bookings/BookingDetail.jsx
+++ src/features/bookings/BookingDetail.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/jsx-no-leaked-render */
 import styled from "styled-components";
 
 import BookingDataBox from "./BookingDataBox";
@@ -11,6 +12,7 @@ import ButtonText from "../../ui/ButtonText";
 import { useMoveBack } from "../../hooks/useMoveBack";
 import Spinner from "../../ui/Spinner";
 import { useBooking } from "./useBooking";
+import { useNavigate } from "react-router-dom";
 
 const HeadingGroup = styled.div`
   display: flex;
@@ -19,6 +21,7 @@ const HeadingGroup = styled.div`
 `;
 
 function BookingDetail() {
+  const navigate = useNavigate();
   const { booking, isLoading } = useBooking();
 
   const moveBack = useMoveBack();
@@ -46,6 +49,11 @@ function BookingDetail() {
       <BookingDataBox booking={booking} />
 
       <ButtonGroup>
+        {status === "unconfirmed" && (
+          <Button onClick={() => navigate(`/checkin/${bookingId}`)}>
+            Check in
+          </Button>
+        )}
         <Button variation="secondary" onClick={moveBack}>
           Back
         </Button>
